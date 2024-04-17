5eb2d1624eb1e56a0b9ae7be1924c35c5c02ee1b
exlomex <sergik5849@gmail.com>
2024-04-17 08:26:00 +0300

    add public api for shared layer

---
+++ src/shared/layouts/IconLayout/index.ts
@@ -0,0 +1,1 @@
+export { IconLayout } from './IconLayout';
--- src/widgets/AsideMenu/ui/AsideMenu.tsx
+++ src/widgets/AsideMenu/ui/AsideMenu.tsx
@@ -1,8 +1,8 @@
 import { classNames } from '@/shared/lib/classNames';
 import { FC } from 'react';
-import { IconLayout } from '@/shared/layouts/IconLayout/IconLayout';
-import { Input } from '@/shared/ui/Input/Input';
 import SettingSvg from '@/shared/assets/settings.svg';
+import { IconLayout } from '@/shared/layouts/IconLayout';
+import { Input } from '@/shared/ui/Input';
 import cls from './AsideMenu.module.scss';
 
 interface AsideMenuProps {
@@ -13,7 +13,6 @@ export const AsideMenu: FC = ({ className }: AsideMenuProps) => (
     <aside className={classNames(cls.AsideMenu, {}, [className])}>
         <div>
             <Input placeholder={'Поиск'} />
-            <div>скрыть</div>
         </div>
         <IconLayout
             Svg={SettingSvg}
